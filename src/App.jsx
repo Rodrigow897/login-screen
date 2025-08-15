c66214b7b408da4a53a5fe51667461a55f5206e2
Rodrigo <rwesley2000@gmail.com>
2025-08-15 19:07:06 -0300

    style: removed from and added style for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import Logo from '../public/Logo.svg'
-import LogoDireito from '../public/login-illustrator.svg'
-import LogoEmail from '../public/logo-email.svg'
-import LogoPassword from '../public/logo-password.svg'
 import Button from './components/login-button.jsx'
 import './App.css'
 import InputBox from './components/input-box.jsx'
@@ -11,37 +7,35 @@ import React from 'react'
 const App = () => (
   <div className='container'>
     <div className="lado-esquerdo">
-      <img id='logo' src={Logo} alt="foto da logo" />
+      <img id='logo' src='../public/Logo.svg' alt="foto da logo" />
       <h1>login into your account</h1>
 
-      <form onSubmit={handleSubmit}>
-        <InputBox
-          className = "input-box1"
-          subtitle = "Email Adress"
-          type = "text"
-          placeholder = "alex@gmail.com"
-          id = "logo-email"
-          src = {LogoEmail}
-          alt = "logo email"
-        />
+      <InputBox
+        className = "input-box1"
+        subtitle = "Email Adress"
+        type = "text"
+        placeholder = "alex@gmail.com"
+        id = "logo-email"
+        src = '../public/logo-email.svg'
+        alt = "logo email"
+      />
 
-        <InputBox
-          className = "input-box2"
-          subtitle = "Password"
-          type = "password"
-          placeholder = "Enter your password"
-          id = "logo-password"
-          src = {LogoPassword}
-          alt = "logo password"
-        />
+      <InputBox
+        className = "input-box2"
+        subtitle = "Password"
+        type = "password"
+        placeholder = "Enter your password"
+        id = "logo-password"
+        src = '../public/logo-password.svg'
+        alt = "logo password"
+      />
 
-        <a href="" className='link'>Forgot Password?</a>
+      <a href="" className='link'>Forgot Password?</a>
 
-        <Button 
-          className = "buttonLogin"
-          title = "Login Now"
-        />
-      </form>
+      <Button 
+        className = "buttonLogin"
+        title = "Login Now"
+      />
 
       <div class="divider">
         <span>OR</span>
@@ -55,7 +49,7 @@ const App = () => (
     </div>
 
     <div className="lado-direito">
-      <img src={LogoDireito} alt="foto logo" />
+      <img src='../public/login-illustrator.svg' alt="foto logo" />
     </div>
   </div>
 )
